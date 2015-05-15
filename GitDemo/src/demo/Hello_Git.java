e5a2ea2490ccf08bcb175be7d161ddb98bc4fc5b
Thomas Patterson <tompatt92@gmail.com>
2015-05-15 15:54:47 -0400

    Initial Commit

---
+++ GitDemo/src/demo/Hello_Git.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package demo;
+
+/**
+ * @author thomas.patterson
+ *
+ */
+public class Hello_Git {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Git is fun");
+
+	}
+
+}
